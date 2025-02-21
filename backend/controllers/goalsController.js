37360f317e932a698775441a2ef18d72776d0c20
Muhammad Taha <39196340+Tahawahid@users.noreply.github.com>
2025-02-22 00:27:16 +0500

    Goals API setup is done

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,22 +1,43 @@
 import asyncHandler from 'express-async-handler';
+import Goal from '../models/goalModel.js';
 
 const getGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({ message: 'Get goals' });
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 const setGoals = asyncHandler( async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please enter a goal')
-    }    
-    res.status(200).json({ message: 'Set goals' });
+    }
+    const goal = new Goal({
+        text: req.body.text
+    });
+
+    const createdGoal = await goal.save();
+    res.status(201).json(createdGoal);
 });
 
 const updateGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({ message: `Update goals ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler( async (req, res) => {
+    const goal = await Goal.findById(req.params.id);
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.deleteOne();
     res.status(200).json({ message: `Delete goals ${req.params.id}` });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import colors from 'colors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import goalsRoutes from './routes/goalsRoutes.js';
 
@@ -7,6 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', goalsRoutes);
